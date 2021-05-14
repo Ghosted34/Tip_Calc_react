fec0629bedca3b5c568a5cd7d360403df6411fce
Ghosted34 <greyman1934@outlook.com>
2021-05-14 01:15:50 +0100

    validation

--- src/Components/FormLabels/BillInput.jsx
+++ src/Components/FormLabels/BillInput.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BillInput =(props)=>{
     return( <label>
         Bill total:
-        <input value = {props.billValue} onChange={props.billChange}/>
+        <input value = {props.billValue} onChange={props.billChange} type="number" step="0.01" title="Eg. 300.75"/>
       </label>)
 }
 
--- src/Components/FormLabels/PartySize.jsx
+++ src/Components/FormLabels/PartySize.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PartySize =(props)=>{
     return <label>
         Party size:
-        <input value={props.partySize} onChange={props.sizeChange}/>
+        <input value={props.partySize} onChange={props.sizeChange} type="number" step="1" title="Nummbers only, Please"/>
     </label>
 }
 
